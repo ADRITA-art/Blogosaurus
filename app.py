3b23d2128e2a304a56e579eef0023af9fb74a238
ADRITA-art <chakrabortyadrita04@gmail.com>
2025-04-27 16:14:09 +0530

    Wsgi serving

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 # This file redirects to the app package to maintain compatibility
 from app import create_app, db, migrate
 
+# Create an instance of the app for Gunicorn to use
+app = create_app()
+
 # This allows imports like "from app import db" to work
 # while still using the proper package structure
